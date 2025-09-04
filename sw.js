3656a3ce357d290a249d7924432a50f336569c70
Mo9152818948 <r87425902@gmail.com>
2025-09-05 03:15:01 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,29 @@
-const CACHE_NAME='passgen-v1';
-const ASSETS=[
+const CACHE_NAME = 'passgen-v2'; // نسخه را بالا بردیم تا کش تازه شود
+const ASSETS = [
   './',
-  './index_with_backgrounds.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
+  './index.html',
+  './manifest.webmanifest'
 ];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>
-    Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))
-  ));
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
-self.addEventListener('fetch',e=>{
-  const url=new URL(e.request.url);
-  if(url.origin===location.origin){
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(res => res || fetch(e.request))
+    );
   }
 });
