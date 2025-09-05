12ccddd276c0c47e1229552730003095990e41ed
Mo9152818948 <r87425902@gmail.com>
2025-09-05 16:44:07 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,18 @@
-const CACHE_NAME = 'passgen-v3'; // نسخه جدید
+const CACHE_NAME = 'passgen-v9'; // برای رفرش کش، هر بار عدد رو بیشتر کن
 const ASSETS = [
   './',
   './index.html',
-  './manifest.webmanifest'
+  './manifest.webmanifest',
+  './1000002828.png', // 192
+  './1000002819.png'  // 512
 ];
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-self.addEventListener('activate', (e) => {
+
+self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
@@ -17,9 +20,7 @@ self.addEventListener('activate', (e) => {
   );
   self.clients.claim();
 });
-self.addEventListener('fetch', (e) => {
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin) {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-  }
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
